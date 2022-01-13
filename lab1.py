ce4551916224d844c6679a62398c3054eb0cee29
hellosusan <ngocthao@ualberta.ca>
2022-01-12 20:19:58 -0700

    curl step 10

--- lab1.py
+++ lab1.py
@@ -4,7 +4,11 @@ import requests
 #print(requests.__version__)
 
 # curl step 5:
-response = requests.get('http://www.google.com/')
-print("Status Code: ", response.status_code)
-print("Body:")
+# response = requests.get('http://www.google.com/')
+# print("Status Code: ", response.status_code)
+# print("Body:")
+# print(response.text)
+
+# curl step 10:
+response = requests.get('https://raw.githubusercontent.com/hellosusan/cmput404-labs/main/lab1.py')
 print(response.text)
